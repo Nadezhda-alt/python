377f7ae950c6d7d2b84edba3e18f5ec052b1122e
Nadezhda-alt <severnyy01@gmail.com>
2020-12-06 19:04:53 +0300

    1 задание на питоне

---
+++ 1 задание.py
@@ -0,0 +1,11 @@
+number_1 = input("Введите первое число")
+number_2 = input("Введите второе число")
+if number_1 < number_2:
+    print(number_1 + "Меньше чем" + number_2)
+else:
+    print(number_1 + "Больше чем" + number_2)
+if number_1 == number_2:
+    print("Числа равны")
+else:
+    print("Числа не равны")
+print("Конец первого задания 1 урока")
